4f744060bcf8a5d0b5c16fced3a0776cdd2563b6
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-10-19 14:17:58 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,3 @@
--- Script: task_4.sql
--- Purpose: Print the full description of the table 'books' 
--- from the database 'alx_book_store' without using DESCRIBE or EXPLAIN
-
+-- Task 4: Display the full table description without DESCRIBE or EXPLAIN
 USE alx_book_store;
-
 SHOW CREATE TABLE books;
