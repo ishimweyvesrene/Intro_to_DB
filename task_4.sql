6be49202ee13228be52b068c425c0a8447f8d18b
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-10-19 17:11:11 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
+-- Task 4: Print the full description of the table Books
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
-
+  AND TABLE_NAME = 'Books';
