6cd6027387f4a7c95f6b1948bda73770da7911e1
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-10-19 14:14:19 +0200

    Add files via upload

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Script: task_4.sql
+-- Purpose: Print the full description of the table 'books' 
+-- from the database 'alx_book_store' without using DESCRIBE or EXPLAIN
+
+USE alx_book_store;
+
+SHOW CREATE TABLE books;
